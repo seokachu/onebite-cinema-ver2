94d2f5306555812713ff4ee0aa82584e3ce2f9d1
Seoyoung Park <seokachuu@gmail.com>
2025-04-04 17:14:09 +0900

    [Fix] dummy data 수정

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -2,18 +2,17 @@ import style from "./page.module.css";
 import Image from "next/image";
 
 const movie = {
-  id: 533535,
-  title: "데드풀과 울버린",
-  releaseDate: "2024-07-24",
-  company:
-    "Marvel Studios, Maximum Effort, 21 Laps Entertainment, 20th Century Studios, Kevin Feige Productions, TSG Entertainment",
-  genres: ["액션", "코미디", "SF"],
-  subTitle: "",
+  id: 17,
+  title: "백설공주",
+  releaseDate: "2025-03-19",
+  company: "월트디즈니 컴퍼니 코리아",
+  genres: ["판타지", "뮤지컬"],
+  subTitle: "동화 그 이상의 마법 같은 이야기",
   description:
-    "히어로 생활에서 은퇴한 후, 평범한 중고차 딜러로 살아가던 ‘데드풀’이 예상치 못한 거대한 위기를 맞아 모든 면에서 상극인 ‘울버린’을 찾아가게 되며 펼쳐지는 도파민 폭발 액션 블록버스터.",
-  runtime: 128,
+    "눈보라가 몰아치던 겨울 밤 태어난 백설공주. 온정이 넘치던 왕국에서 모두의 사랑을 받았지만, 강력한 어둠의 힘으로 왕국을 빼앗은 여왕의 위협에 숲으로 도망친다. 마법의 숲에서 간신히 살아남은 백설공주는 신비로운 일곱 광부들과 만나게 되며 새로운 세상을 마주하고, 마음속 깊이 숨겨진 용기와 선한 힘을 깨닫게 된다. 그리고 마침내, 빼앗긴 왕국을 되찾기 위해 여왕과 맞서 싸우기로 결심하는데…",
+  runtime: 109,
   posterImgUrl:
-    "https://media.themoviedb.org/t/p/w300_and_h450_face/4Zb4Z2HjX1t5zr1qYOTdVoisJKp.jpg",
+    "https://search.pstatic.net/common?quality=75&direct=true&src=https%3A%2F%2Fmovie-phinf.pstatic.net%2F20250319_123%2F1742370056144UwYpI_JPEG%2Fmovie_image.jpg",
 };
 
 export default function Page() {
